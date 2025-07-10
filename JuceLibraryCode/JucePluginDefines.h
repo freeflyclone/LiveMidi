15abd90e4d2e7bab3593ed118953b2bc2bf04cc1
Evan Mortimore <evan-github@madstyle.net>
2025-07-10 09:39:59 -0700

    Add LogWindow class to Projucer and it's subsequent generated files.

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -155,7 +155,7 @@
  #define JucePlugin_ARAFactoryID           "com.Lumenicious.LiveMidi.factory"
 #endif
 #ifndef  JucePlugin_ARADocumentArchiveID
- #define JucePlugin_ARADocumentArchiveID   "com.Lumenicious.LiveMidi.aradocumentarchive.1.0.0"
+ #define JucePlugin_ARADocumentArchiveID   "com.Lumenicious.LiveMidi.aradocumentarchive.0.1.0"
 #endif
 #ifndef  JucePlugin_ARACompatibleArchiveIDs
  #define JucePlugin_ARACompatibleArchiveIDs  ""
