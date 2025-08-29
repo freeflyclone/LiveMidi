5e62d78f4bd38cf4b0c29533ca310d6b4046157c
Evan Mortimore <evan-github@madstyle.net>
2025-08-29 14:21:14 -0700

    GrooveBrowser UX development beginning, now that I've created a useful hierarchy of GrooveFolders.  (I think)

--- Source/GrooveBrowser.cpp
+++ Source/GrooveBrowser.cpp
@@ -13,13 +13,13 @@
 
 GrooveBrowser::GrooveBrowser()
 {
-    addAndMakeVisible(mListBoxes[0]);
+    int xOffset{ 0 };
 
-    addAndMakeVisible(mListBoxes[1]);
-    mListBoxes[1].setTopLeftPosition(GROOVE_LISTBOX_WIDTH-1, 0);
-
-    addAndMakeVisible(mListBoxes[2]);
-    mListBoxes[2].setTopLeftPosition((GROOVE_LISTBOX_WIDTH-1) * 2, 0);
+    for (auto& listBox : mListBoxes) {
+        addChildComponent(listBox);
+        listBox.setTopLeftPosition(xOffset, 0);
+        xOffset += GROOVE_LISTBOX_WIDTH - 1;
+    }
 }
 
 GrooveBrowser::~GrooveBrowser() {
@@ -30,21 +30,23 @@ void GrooveBrowser::Initialize(File f) {
 
     auto maxDepth(mStore.MaxDepth());
 
-    for (auto& listBox : mListBoxes)
+    for (auto& listBox : mListBoxes) {
         listBox.clear();
+        listBox.setVisible(false);
+    }
 
     mStore.Enumerate([&](GrooveFolder& gf) {
         auto& fileSelf = gf.GetSelfFile();
 
-        MYDBG("enumerating: " + fileSelf.getFileName().toStdString());
-
         for (const auto child : gf.GetChildren()) {
+            mListBoxes[0].setVisible(true);
             mListBoxes[0].add(child->GetSelfFile().getFileName());
-            MYDBG("  " + child->GetSelfFile().getFileName().toStdString() + ", parent: " + child->GetParent()->GetSelfFile().getFileName().toStdString());
         }
 
-        for (const auto fileName : gf.GetFileNames())
-            MYDBG("    " + fileName.toStdString());
+        for (const auto fileName : gf.GetFileNames()) {
+            mListBoxes[0].setVisible(true);
+            mListBoxes[0].add(fileName);
+        }
     });
 
     for (auto& listBox : mListBoxes)
