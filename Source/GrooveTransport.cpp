5060819f90fe47902c12a63f2d64614a3a9ee2c3
Evan Mortimore <evan-github@madstyle.net>
2025-09-06 15:41:38 -0700

    Fleshing out GrooveTransport to be real AudioPlayHead. (hopefully)

--- Source/GrooveTransport.cpp
+++ Source/GrooveTransport.cpp
@@ -13,3 +13,22 @@
 Optional<AudioPlayHead::PositionInfo> GrooveTransport::getPosition() const {
     return mPositionInfo;
 }
+
+void GrooveTransport::actionListenerCallback(const String& message) {
+    GrooveActionMessage gam = json::parse(message.toStdString());
+    if (gam["component"] != "GTPT")
+        return;
+
+    auto action = gam["action"];
+
+    if (action == "PLAY") {
+        mIsPlaying = true;
+    }
+    else if (action == "RECORDING") {
+        mIsRecording = true;
+    }
+    else if (action == "STOP") {
+        mIsPlaying = false;
+        mIsRecording = false;
+    }
+}
--- Source/GrooveTransport.h
+++ Source/GrooveTransport.h
@@ -11,16 +11,25 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include "GrooveActionMessage.h"
+#include "Log.h"
 
 using namespace juce;
 
-class GrooveTransport : public AudioPlayHead {
+class GrooveTransport : public AudioPlayHead, public ActionListener {
 public:
     GrooveTransport() {};
     ~GrooveTransport() {};
 
     Optional<PositionInfo> getPosition() const;
+    bool canControlTransport() override { return true; }
+    void transportPlay(bool shouldStart) { mIsPlaying = shouldStart; };
+    void transportRecord(bool shouldStart) { mIsRecording = shouldStart; };
 
 private:
     PositionInfo mPositionInfo;
+    bool mIsPlaying{ false };
+    bool mIsRecording{ false };
+
+    void actionListenerCallback(const String&);
 };
